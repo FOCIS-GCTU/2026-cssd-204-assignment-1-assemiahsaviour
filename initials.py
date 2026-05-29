90088bc7703eb7b34b5e920e0d06ae375c14fd02
assemiahsaviour <2425403653@live.gctu.edu.gh>
2026-05-29 15:40:44 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Assemiah Saviour Yaa Owusu
+# STUDENT ID:  2425403653
+# Email: 2425403653@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Assemiah Saviour Yaa Owusu, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials ASO in stylized large block letters using A, S, and O characters with periods.
 
+    print()
+    print("...ASO")
+    print()
+    print(".......AAAA.............SSSSSSSSSS..........OOOOOOOOOO......")
+    print("......AAAAAA...........SS..................OO........OO.....")
+    print(".....AA....AA..........SS..................OO........OO.....")
+    print(".....AA....AA...........SSSSSSSSSS.........OO........OO.....")
+    print(".....AAAAAAAA....................SS........OO........OO.....")
+    print(".....AAAAAAAA....................SS........OO........OO.....")
+    print(".....AA....AA....................SS........OO........OO.....")
+    print(".....AA....AA.....**.............SS...**...OO........OO...**")
+    print(".....AA....AA.....**....SSSSSSSSSS....**....OOOOOOOOOO....**")
+    print("............................................................")
+    print()
 
 
 main()
